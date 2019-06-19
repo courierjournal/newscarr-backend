b6ebf988f5e287702693417ccd07b51784c5f1ef
Jesse Hazel <jwhazel@jesses-imac.lan>
2019-06-19 08:54:07 -0400

    Add Bookmarks controller

--- app/Http/Controllers/BookmarksController.php
+++ app/Http/Controllers/BookmarksController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class ContactsController extends Controller
+class BookmarksController extends Controller
 {
+    private $db;
 
     public function __construct()
     {
-        config(["database.connections.mysql" => [
-            "database" => "contacts"
-        ]]);
+        config(['database.connections.privatedb.database' => 'bookmarks']);
+        $this->db = DB::connection('privatedb');
     }
 
     /**
@@ -22,27 +22,19 @@ class ContactsController extends Controller
      */
     public function getList(Request $request)
     {
-        $query = 'SELECT incidents.id, incidents.address, incidents.date, incidents.narrative, COUNT(victims.incident_id) as victimCount FROM incidents LEFT JOIN victims ON incidents.id=victims.incident_id GROUP BY incidents.id ORDER BY incidents.date DESC';
-        $list = DB::select($query);
-        foreach ($list as $index => $row) {
-            if (strlen($row->narrative) > 50) {
-                $list[$index]->narrative = substr($row->narrative, 0, self::MAXNARRATIVE) . "...";
-            }
-        }
+        $query = 'SELECT * FROM bookmarks ORDER BY category ASC, name ASC';
+        $list = $this->db->select($query);
         return response()->json($list);
+    }
+
+    public function getFullList(Request $request){
+
     }
 
 
     public function upsertRecord(Request $request)
     {
-        //Upsert incident
-        DB: insert('INSERT INTO incidents (id, date, time, address, city, state, zip, lat, lng, narrative, notes, guns, source, story) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
-        //Upsert victims
-        foreach ($victim as $request->post->victims) { }
-        //Upsert suspects
-        foreach ($suspect as $request->post->suspects) { }
 
-        return "Message from the controller - Full body is here " . json_encode($request->post());
     }
 
     public function deleteRecord()
